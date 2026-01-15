87f9e64069c2b6057b9cc1194acf81f1a6297f6b
gmitsolution3 <gmitsolution3@gmail.com>
2026-01-15 11:34:18 +0600

    chore: removed time and date slot module for new approach

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import defaultController from "../modules/default/default.controller";
 
 import timeslotRoute from "../modules/time-slot/time-slot.route";
+import dateslotRoute from "../modules/date-slot/date-slot.route";
 import doctorServiceRoute from "../modules/doctor-service/doctor-service.route";
 
 const router = Router();
@@ -9,5 +10,6 @@ const router = Router();
 router.get("/", defaultController);
 router.use("/doctor-service", doctorServiceRoute);
 router.use("/time-slot", timeslotRoute);
+router.use("/date-slot", dateslotRoute);
 
 export default router;
